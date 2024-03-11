599a5e8b92987b1e54951d7e62e2171e52afe31c
Mozu <littlewyverngx@gmail.com>
2024-03-10 18:27:45 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GTActionSender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
